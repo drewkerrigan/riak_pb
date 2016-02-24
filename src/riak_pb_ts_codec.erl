70abdbf783263c9cca550c434a7787d8cd63d7be
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-02-24 14:25:46 +0100

    fixing clause

--- src/riak_pb_ts_codec.erl
+++ src/riak_pb_ts_codec.erl
@@ -126,7 +126,7 @@ decode_cells(Cells) ->
 -spec encode_row(list(tscolumntype()), list(ldbvalue())) -> #tsrow{}.
 encode_row(ColumnTypes, RowCells) when length(ColumnTypes) =:= length(RowCells) ->
     #tsrow{cells = [encode_cell(ColumnTypeCell) || ColumnTypeCell <- lists:zip(ColumnTypes, RowCells)]};
-encode_row(_ColumnTypes, []) ->
+encode_row(_ColumnTypes, [[]]) ->
     #tsrow{}.
 
 %% @doc Only for encoding rows for PUTs on the erlang client.
